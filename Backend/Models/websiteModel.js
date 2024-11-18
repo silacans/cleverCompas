2f9e8330a8e31d6b7dcfbe56c20ed4197e34d7c3
Gaganjit Virdi <gaganjitvirdi@MacBook-Pro-65.local>
2024-11-17 16:10:16 -0800

    connected to remote db using mongdo db remote dataBase

---
+++ Backend/Models/websiteModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const websiteSchema = new Schema({
+  subject: {
+    type: String,
+    required: false //e.g. Math, Science. History, choose what is applicable
+  },
+  username: {
+    type: String,
+    required: true //name of the user, then filters the userinto their role
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['student', 'tutor', 'parent']
+  },
+  duration: {
+    type: Number,
+    required: true // duration of the session in hours
+  },
+  date: {
+    type: Date,
+    required: true // date of the tutoring session
+  },
+  email: {
+    type: String,
+    required: true, // email of tutor/student/parent 
+    unique: true //makes sure there aren't duplicates in the Database
+  },
+  password: {
+    type: String,
+    required: true, //Password
+  },
+  phoneNumber: {
+    type: String,
+    required: false // phone number is optional
+  },
+  ratePerHour: {
+    type: Number,
+    required: function () { return this.role === 'tutor'; }, // Only required for tutors
+    validate: {
+      validator: function (value) {
+        // Ensure ratePerHour is only set for tutors
+        return this.role === 'tutor' || value == null;
+      },
+      message: 'Only tutors can have a ratePerHour.'
+    }
+  }
+}, { timestamps: true })
+
+
+module.exports = mongoose.model('Website', websiteSchema)
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 
+
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes/routes.js') 
 
 //creating an express app
@@ -16,11 +18,21 @@ app.use((req, res, next) => {
 //route handler
 app.use('/api/routes', routes)
 
-
-//listing for request 
+// connecting to DB
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    //listing for request once connected to the DB
 app.listen(process.env.PORT, () => {
-console.log('Listing port', process.env.PORT)
+    console.log('Connected to DB, Listing port', process.env.PORT)
+    
+    }) 
+    
 
-}) 
+
+})
+.catch((error) => {
+    console.log(error)
+})
+
 
 
