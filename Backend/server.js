8e7e70202fd0e2e974f77ba967099f5094531fe2
Gaganjit Virdi <gaganjitvirdi@MacBook-Pro-65.local>
2024-11-17 16:57:29 -0800

    connected the db with models and schemeas and tested the routes and the post API

--- Backend/server.js
+++ Backend/server.js
@@ -4,10 +4,12 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes/routes.js') 
+const routes = require('./routes/website.js') 
 
 //creating an express app
 const app = express()
+
+app.use(express.json())
 
 //middleware
 app.use((req, res, next) => {
@@ -16,7 +18,7 @@ app.use((req, res, next) => {
 })
 
 //route handler
-app.use('/api/routes', routes)
+app.use('/api/website', routes)
 
 // connecting to DB
 mongoose.connect(process.env.MONGO_URI)
