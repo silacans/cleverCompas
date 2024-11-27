566b8ebba642fdebe9276e2913b2dbea6072f318
Gaganjit Virdi <gaganjitvirdi@MacBook-Pro-65.local>
2024-11-27 00:30:04 -0800

    added logout function button to sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = ({ children }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate();
 
   // Automatically collapse the sidebar when the location changes
   useEffect(() => {
@@ -15,6 +16,13 @@ const Sidebar = ({ children }) => {
     setIsCollapsed(!isCollapsed);
   };
 
+
+  // This is for the logout button functions
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Remove the token
+    navigate('/login'); // Redirect to login page
+  };
+
   return (
     <div className="layout-container">
       <aside className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
@@ -22,11 +30,16 @@ const Sidebar = ({ children }) => {
           {isCollapsed ? '>' : '<'}
         </button>
         {!isCollapsed && (
+          <>
           <ul className="sidebar-links">
             <li><Link to="/dashboard">Dashboard</Link></li>
             <li><Link to="/schedule">Schedule</Link></li>
-            <li><Link to="/example">Example</Link></li>
+            <li><Link to="/Notification">Notification</Link></li> {/* Need to add pages */}
           </ul>
+          <button className="logout-button" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
         )}
       </aside>
       <main className={`content ${isCollapsed ? 'expanded' : ''}`}>{children}</main>
